03314ed2cb9696985a34457d71213fe12686d7b0
NeilMagon <neil.magon97@gmail.com>
2024-04-30 17:04:30 +0200

    feat: fix badwords.php

--- badwords.php
+++ badwords.php
@@ -1,7 +1,7 @@
 <!-- Descrizione:
 Creare un form PHP che permetta di inviare due campi: un paragrafo ed una parola da censurare.
 Gestire il tutto con due file diversi.
-Il primo file dovrà permettere all’utente di inserire i dati e inviare la richiesta al server.
+Il primo file dovrà permettere all’utente di inserire i dati einv iare la richiesta al server.
 Il secondo file riceverà la richiesta ed eseguirà queste operazioni:
 - stampare a schermo il paragrafo e la sua lunghezza
 - stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi 
@@ -10,8 +10,9 @@ Il secondo file riceverà la richiesta ed eseguirà queste operazioni:
     $paragraph = $_GET['paragraph'];
     $word = $_GET['word'];
     $paragraphLength = strlen($paragraph);
-    $wordCensored = str_replace($word, '***', $word);
-    var_dump($paragraphCensored);
+    $wordCensored = str_replace($word, '***', $paragraph);
+    $wordLength = strlen($word);
+    $lenght = $paragraphLength - $wordLength;
 ?>
 
 <!DOCTYPE html>
@@ -33,6 +34,7 @@ Il secondo file riceverà la richiesta ed eseguirà queste operazioni:
         <h3>Parola censurata</h3>
         <div class="card">
             <p> <?php echo $wordCensored; ?> </p>
+            <p>Lunghezz di: <?php echo $lenght; ?> caratteri</p>
         </div>
     </div>
 </body>
